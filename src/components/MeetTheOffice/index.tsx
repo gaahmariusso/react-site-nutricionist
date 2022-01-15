6a9b6d92da0fea2e005aa5163806304c050bf2bb
Gabriel Mariusso <gabriel_mariusso@hotmail.com>
2022-01-15 16:17:16 -0300

    add consult's iframe360

--- src/components/MeetTheOffice/index.tsx
+++ src/components/MeetTheOffice/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Iframe from 'react-iframe';
+
 const MeetTheOffice: React.FC = () => {
   return (
     <Container>
-      <h1>MeetTheOffice</h1>
+      <Iframe
+        url="https://360player.io/p/twb2Yz/"
+        width="800"
+        height="500"
+        allow="fullscreen; xr-spatial-tracking"
+        data-token="twb2Yz"
+        id="consult"
+        className="consult"
+        position="relative" />
     </Container>
   );
 };
--- src/components/MeetTheOffice/styles.ts
+++ src/components/MeetTheOffice/styles.ts
@@ -2,4 +2,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 
+display: flex;
+justify-content: center;
+
+ @media (max-width: 1440px) {
+
+    margin-top: 5%;
+  }
+  
+  @media (max-width: 1015px) {
+  
+    margin-top: 5%;
+  }
+
 `;
